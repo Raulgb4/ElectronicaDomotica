817a66ccda2ef303bfd190d73a74bb246ce75cdf
RaulgbUMA <raulgarciabalongoes@gmail.com>
2025-02-18 12:53:07 +0100

    Probando el gitignore

--- PracticaPrueba/src/main.cpp
+++ PracticaPrueba/src/main.cpp
@@ -19,8 +19,8 @@ void loop()
   // Encender el LED
   digitalWrite(LED_PIN, HIGH);
   
-  // Esperar un segundo
-  delay(1000);
+  // Esperar dos segundos
+  delay(2000);
   
   // Apagar el LED
   digitalWrite(LED_PIN, LOW);
