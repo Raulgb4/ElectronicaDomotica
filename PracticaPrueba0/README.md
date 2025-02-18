18c7cf77514c53c446c38dd974c159945e23121d
RaulgbUMA <raulgarciabalongoes@gmail.com>
2025-02-18 18:23:30 +0100

    Práctica de prueba 0 funcionando

---
+++ PracticaPrueba0/README.md
@@ -0,0 +1,11 @@
+# 🔴 Práctica de prueba 0: Blink - Parpadeo de LED en Arduino
+
+Este proyecto hace que el **LED integrado de Arduino** parpadee continuamente, encendiéndose durante dos segundos y apagándose durante un segundo.
+
+## 📜 Descripción
+
+Este es un programa básico para Arduino que enciende y apaga un **LED de manera repetitiva** con intervalos de tiempo. Es útil para entender cómo funciona el control de **salidas digitales** en Arduino.
+
+## 🛠️ Material necesario
+
+- Una placa **Arduino** (UNO, Mega, Nano, etc.)
---
+++ PracticaPrueba0/src/main.cpp
@@ -0,0 +1,69 @@
+/**
+ * Blink (Parpadeo infinito) del LED de la placa Arduino UNO
+ *
+ * Enciende el LED durante un segundo,
+ * luego lo apaga durante un segundo, y repite el ciclo.
+ * 
+*/
+
+/*
+Importa la librería de Arduino, que permite usar funciones como 
+pinMode(), digitalWrite(), y delay().
+Sin esta librería, Arduino no entendería las funciones 
+específicas de hardware.
+*/
+#include "Arduino.h" // Omitir esta línea al importar en TinkerCAD
+
+/*
+Define una constante llamada LED_PIN, 
+que representa el LED integrado en la placa
+(normalmente conectado al pin 13 en un Arduino UNO).
+
+LED_BUILTIN es un valor predefinido que ya 
+indica el número del pin correcto.
+*/
+#define LED_PIN LED_BUILTIN // Usamos el LED integrado en la placa
+
+/*
+La función setup es una función especial de Arduino que
+se ejecuta solo una vez al inicio del programa
+*/
+void setup()
+{
+  /*
+  Configurar el pin 13 (LED) como salida
+  para que Arduino lo controle para encender
+  o apagar el LED
+  */
+  pinMode(LED_PIN, OUTPUT);
+}
+
+/*
+La función loop es una función especial de Arduino que
+se ejecuta infinitamente en bucle
+¿Por qué sucede esto?
+Arduino no tiene un sistema operativo como un ordenador, 
+por lo que siempre ejecuta el código en bucle hasta que 
+se apaga o se sube otro programa
+*/
+void loop()
+{ 
+  /*
+  Encender el LED, enviando una señal digital HIGH (5 voltios) 
+  a un pin (configurado como OUTPUT) de la placa
+  */
+  digitalWrite(LED_PIN, HIGH);
+  
+  /*
+  Esperar dos segundos (2000 milisegundos) antes de continuar
+  Funciona deteniendo la ejecución del programa
+  */ 
+  delay(2000);
+  
+  // Apagar el LED, LOW significa 0 voltios
+  digitalWrite(LED_PIN, LOW);
+  
+  // Esperar un segundo antes de repetir el ciclo
+  delay(1000);
+}
+
